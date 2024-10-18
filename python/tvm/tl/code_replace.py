365af5ee5569acb522d52f2e3f2cb51ab77d54e9
Yu Cheng <yu.cheng@pku.edu.cn>
2024-10-18 04:05:38 +0000

    [tl] Add callback function

---
+++ python/tvm/tl/code_replace.py
@@ -0,0 +1,12 @@
+import re
+import tvm
+# from tvm.experiments.dequant_fp4_gemm import replace
+# from tvm.experiments.mamba2_chunk_scan import replace
+# from tvm.experiments.mamba2_chunk_state import replace
+
+@tvm.register_func(func_name="tvm_callback_cuda_postproc", override=True)
+def tvm_callback_cuda_postproc(code, _):
+    # with open("/home/msra/cy/tvm/python/tvm/tl/modified_code.cu", "r", encoding='utf-8') as f:
+    #     code = f.read()
+    # code = replace(code)
+    return code
