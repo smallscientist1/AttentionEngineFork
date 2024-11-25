51d7a043c22becf85631f5e26f48cb1f42c0c35b
Yu Cheng <yu.cheng@pku.edu.cn>
2024-11-25 02:03:56 -0800

    update conv

--- tl_scripts/conv_example.py
+++ tl_scripts/conv_example.py
@@ -5,9 +5,10 @@ import tvm.tl.language as T
 from functools import partial
 
 
-def convolution(N, C, H, W, INC, KW, KH, P, S, D):
-    INH = (H - 1) * S + (KH - 1) * D + 1 - 2 * P
-    INW = (W - 1) * S + (KW - 1) * D + 1 - 2 * P
+def convolution(N, C, H, W, F, K, S, D, P):
+    KH, KW = K, K
+    OH = (H + 2 * P - D * (K - 1) - 1) // S + 1
+    OW = (W + 2 * P - D * (K - 1) - 1) // S + 1
 
     dtype = "float16"
     accum_dtype = "float"
@@ -17,11 +18,11 @@ def convolution(N, C, H, W, INC, KW, KH, P, S, D):
 
     @T.prim_func
     def main(
-        data: T.Buffer((N, INH, INW, INC), dtype),
-        kernel: T.Buffer((KH, KW, INC, C), dtype),
-        out: T.Buffer((N, H, W, C), dtype),
+        data: T.Buffer((N, H, W, C), dtype),
+        kernel: T.Buffer((KH, KW, C, F), dtype),
+        out: T.Buffer((N, OH, OW, F), dtype),
     ):
-        with T.Kernel(T.ceildiv(C, block_N), T.ceildiv(N * H * W, block_M), threads=128 * 2) as (
+        with T.Kernel(T.ceildiv(F, block_N), T.ceildiv(N * OH * OW, block_M), threads=128 * 2) as (
             bx,
             by,
         ):
@@ -30,8 +31,8 @@ def convolution(N, C, H, W, INC, KW, KH, P, S, D):
             out_local = T.alloc_fragment((block_M, block_N), accum_dtype)
             out_shared = T.alloc_shared((block_M, block_N), dtype)
 
-            kernel_flat = T.Buffer((KH * KW * INC, C), dtype, kernel.data)
-            out_flat = T.Buffer((N * H * W, C), dtype, out.data)
+            kernel_flat = T.Buffer((KH * KW * C, F), dtype, kernel.data)
+            out_flat = T.Buffer((N * OH * OW, F), dtype, out.data)
 
             T.annotate_layout({
                 out_shared: tl.layout.make_swizzled_layout(out_shared),
@@ -40,8 +41,22 @@ def convolution(N, C, H, W, INC, KW, KH, P, S, D):
             })
 
             T.clear(out_local)
-            for k_iter in T.Pipelined(T.ceildiv(KH * KW * INC, block_K), num_stages=4):
+            for k_iter in T.Pipelined(T.ceildiv(KH * KW * C, block_K), num_stages=4):
                 T.c2d_im2col(data, data_shared, by, k_iter, KH, S, D, P)
+                # for i, j in T.Parallel(block_M, block_K):
+                #     k = k_iter * block_K + j
+                #     m = by * block_M + i
+                #     access_h = m % (OH * OW) // OW * S + k // (KW * C) * D - P
+                #     access_w = m % OW * S + k // C % KW * D - P
+                #     in_bound = (
+                #         (access_h >= 0)
+                #         and (access_w >= 0)
+                #         and (access_h < H)
+                #         and (access_w < W)
+                #     )
+                #     data_shared[i, j] = T.if_then_else(
+                #         in_bound, data[m // (OH * OW), access_h, access_w, k % C], 0
+                #     )
                 T.copy(kernel_flat[k_iter * block_K, bx * block_N], kernel_shared)
                 T.gemm(data_shared, kernel_shared, out_local)
 
@@ -60,15 +75,19 @@ def ref_program(A, B, stride, padding, dilation):
 
 
 if __name__ == "__main__":
-    # N, C, H, W, INC, KW, KH, P, S, D = 128, 128, 28, 28, 128, 3, 3, 1, 1, 1
-    N, C, H, W, INC, KW, KH, P, S, D = 128, 256, 28, 28, 256, 3, 3, 1, 2, 1
-    program = convolution(N, C, H, W, INC, KW, KH, P, S, D)
+    # N, C, H, W, F, K, S, D, P = 8, 256, 28, 28, 256, 3, 2, 1, 1
+    # N, C, H, W, F, K, S, D, P = 128, 3, 224, 224, 64, 7, 2, 1, 3
+    N, C, H, W, F, K, S, D, P = 128, 256, 56, 56, 512, 1, 2, 1, 0
+
+    program = convolution(N, C, H, W, F, K, S, D, P)
     ref_program = partial(ref_program, stride=S, padding=P, dilation=D)
     mod, params = tl.lower(program)
     mod = tl.Profiler(mod, params, [2], tl.TensorSupplyType.Normal)
     mod.assert_allclose(ref_program, atol=1e-2, rtol=1e-2)
 
-    total_flops = 2 * N * C * H * W * INC * KH * KW
+    OH = (H + 2 * P - D * (K - 1) - 1) // S + 1
+    OW = (W + 2 * P - D * (K - 1) - 1) // S + 1
+    total_flops = 2 * N * C * OH * OW * F * K * K
 
     latency = mod.do_bench(ref_program, warmup=500)
     print("{:.4f} ms".format(latency))
--- tl_scripts/conv_tune.py
+++ tl_scripts/conv_tune.py
@@ -25,9 +25,10 @@ def get_configs():
     ]
     return configs
 
-def convolution(N, C, H, W, INC, KW, KH, P, S, D):
-    INH = (H - 1) * S + (KH - 1) * D + 1 - 2 * P
-    INW = (W - 1) * S + (KW - 1) * D + 1 - 2 * P
+def convolution(N, C, H, W, F, K, S, D, P):
+    KH, KW = K, K
+    OH = (H + 2 * P - D * (K - 1) - 1) // S + 1
+    OW = (W + 2 * P - D * (K - 1) - 1) // S + 1
 
     dtype = "float16"
     accum_dtype = "float"
@@ -38,11 +39,11 @@ def convolution(N, C, H, W, INC, KW, KH, P, S, D):
 
         @T.prim_func
         def main(
-            data: T.Buffer((N, INH, INW, INC), dtype),
-            kernel: T.Buffer((KH, KW, INC, C), dtype),
-            out: T.Buffer((N, H, W, C), dtype),
+            data: T.Buffer((N, H, W, C), dtype),
+            kernel: T.Buffer((KH, KW, C, F), dtype),
+            out: T.Buffer((N, OH, OW, F), dtype),
         ):
-            with T.Kernel(T.ceildiv(C, block_N), T.ceildiv(N * H * W, block_M), threads=thread_num) as (
+            with T.Kernel(T.ceildiv(F, block_N), T.ceildiv(N * OH * OW, block_M), threads=thread_num) as (
                 bx,
                 by,
             ):
@@ -51,8 +52,8 @@ def convolution(N, C, H, W, INC, KW, KH, P, S, D):
                 out_local = T.alloc_fragment((block_M, block_N), accum_dtype)
                 out_shared = T.alloc_shared((block_M, block_N), dtype)
 
-                kernel_flat = T.Buffer((KH * KW * INC, C), dtype, kernel.data)
-                out_flat = T.Buffer((N * H * W, C), dtype, out.data)
+                kernel_flat = T.Buffer((KH * KW * C, F), dtype, kernel.data)
+                out_flat = T.Buffer((N * OH * OW, F), dtype, out.data)
 
                 T.annotate_layout({
                     out_shared: tl.layout.make_swizzled_layout(out_shared),
@@ -60,7 +61,7 @@ def convolution(N, C, H, W, INC, KW, KH, P, S, D):
                     kernel_shared: tl.layout.make_swizzled_layout(kernel_shared),
                 })
                 T.clear(out_local)
-                for k_iter in T.Pipelined(T.ceildiv(KH * KW * INC, block_K), num_stages=num_stages):
+                for k_iter in T.Pipelined(T.ceildiv(KH * KW * C, block_K), num_stages=num_stages):
                     T.c2d_im2col(data, data_shared, by, k_iter, KH, S, D, P)
                     T.copy(kernel_flat[k_iter * block_K, bx * block_N], kernel_shared)
                     T.gemm(data_shared, kernel_shared, out_local)
@@ -86,34 +87,18 @@ if __name__ == "__main__":
     parser.add_argument('--c', type=int, default=128, help='c')
     parser.add_argument('--h', type=int, default=64, help='h')
     parser.add_argument('--w', type=int, default=64, help='w')
-    parser.add_argument('--inc', type=int, default=128, help='inc')
-    parser.add_argument('--kw', type=int, default=3, help='kw')
-    parser.add_argument('--kh', type=int, default=3, help='kh')
-    parser.add_argument('--p', type=int, default=1, help='p')
+    parser.add_argument('--f', type=int, default=128, help='f')
+    parser.add_argument('--k', type=int, default=3, help='k')
     parser.add_argument('--s', type=int, default=1, help='s')
     parser.add_argument('--d', type=int, default=1, help='d')
+    parser.add_argument('--p', type=int, default=1, help='p')
     args = parser.parse_args()
-    N, C, H, W, INC, KW, KH, P, S, D = args.n, args.c, args.h, args.w, args.inc, args.kw, args.kh, args.p, args.s, args.d
-    total_flops = 2 * N * C * H * W * INC * KH * KW
-    best_latency, best_config, ref_latency = convolution(N, C, H, W, INC, KW, KH, P, S, D)
+    N, C, H, W, F, K, S, D, P = args.n, args.c, args.h, args.w, args.f, args.k, args.s, args.d, args.p
+    OH = (H + 2 * P - D * (K - 1) - 1) // S + 1
+    OW = (W + 2 * P - D * (K - 1) - 1) // S + 1
+    total_flops = 2 * N * C * OH * OW * F * K * K
+    best_latency, best_config, ref_latency = convolution(N, C, H, W, F, K, S, D, P)
     print(f"Best latency: {best_latency}")
     print(f"Best TFlops: {total_flops / best_latency * 1e-9}")
     print(f"Best config: {best_config}")
     print(f"Ref TFlops: {total_flops / ref_latency * 1e-9}")
-
-    # # N, C, H, W, INC, KW, KH, P, S, D = 8, 256, 128, 128, 256, 3, 3, 1, 1, 1
-    # N, C, H, W, INC, KW, KH, P, S, D = 128, 128, 56, 56, 256, 3, 3, 1, 1, 1
-    # program = convolution(N, C, H, W, INC, KW, KH, P, S, D)
-    # ref_program = partial(ref_program, stride=S, padding=P, dilation=D)
-    # mod, params = tl.lower(program)
-    # mod = tl.Profiler(mod, params, [2], tl.TensorSupplyType.Integer)
-    # mod.assert_allclose(ref_program)
-
-    # total_flops = 2 * N * C * H * W * INC * KH * KW
-
-    # latency = mod.do_bench(ref_program, n_warmup=10, n_repeat=10, profiler="torch")
-    # print("{:.2f} ms".format(latency))
-    # print("{:.2f} TFlops".format(total_flops / latency * 1e-9))
-    # latency = mod.do_bench(mod, n_warmup=10, n_repeat=10, profiler="torch")
-    # print("{:.2f} ms".format(latency))
-    # print("{:.2f} TFlops".format(total_flops / latency * 1e-9))
