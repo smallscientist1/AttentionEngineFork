ce106141c1b25abf341b8e4250783c5087dea846
Yu Cheng <yu.cheng@pku.edu.cn>
2024-11-30 06:42:03 -0800

    update

--- tl_scripts/dequant_gemm_fp4.py
+++ tl_scripts/dequant_gemm_fp4.py
@@ -4,6 +4,7 @@ from tvm import tl
 from tvm.tl.autotuner import *
 import itertools
 import torch
+import argparse
 
 def interleave_weight(qweight, nbits=4, target_dtype="float16"):
     import numpy as np
@@ -404,22 +405,21 @@ def matmul_(
     num_bits=4,
 ):
     def get_configs():
-        block_M = [64, 128, 256]
+        block_M = [64]
         block_N = [64, 128, 256]
-        block_K = [64, 128]
-        num_stages = [1, 2, 3, 4, 5]
-        thread_num = [128, 256, 512]
+        block_K = [64]
+        num_stages = [2, 3, 4]
+        thread_num = [128, 256]
         _configs = list(itertools.product(block_M, block_N, block_K, num_stages, thread_num))
 
         configs = [
             {'block_M': c[0], 'block_N': c[1], 'block_K': c[2], 'num_stages': c[3], 'thread_num': c[4]}
             for c in _configs
-            if c[4] < c[0] * 2
         ]
         return configs
 
     @autotune(configs=get_configs(), keys=['block_M', 'block_N', 'block_K', 'num_stages', 'thread_num'], warmup=10, rep=5)
-    @jit(out_idx=[2], supply_type=tl.TensorSupplyType.Integer, ref_prog=None)
+    @jit(out_idx=[2], supply_type=tl.TensorSupplyType.Integer, ref_prog=None, profiler="tvm")
     def kernel(block_M=None, block_N=None, block_K=None, num_stages=None, thread_num=None):
         num_elems_per_byte = 8 // num_bits
         storage_dtype = "uint8"
@@ -456,11 +456,15 @@ def matmul_(
                 T.clear(Ct_local)
                 for k in T.Pipelined(
                     T.ceildiv(K, block_K), 
-                    num_stages=num_stages,
-                    order=[-1,-1,0,1],
-                    stage=[-1,-1,0,0],
-                    group=[[0],[1],[2,3,4],[5]]
+                    num_stages=num_stages
                 ):
+                # for k in T.Pipelined(
+                #     T.ceildiv(K, block_K), 
+                #     num_stages=num_stages,
+                #     order=[-1,-1,0,1],
+                #     stage=[-1,-1,0,0],
+                #     group=[[0],[1],[2,3,4],[5]]
+                # ):
                     T.copy(A[by * block_M, k * block_K], A_shared)
                     T.copy(B[bx * block_N, k * block_K // num_elems_per_byte], B_shared)
                     T.copy(B_shared, B_local)
@@ -495,12 +499,7 @@ def tune_gemm(
         dtypeC,
         dtypeAccum
     )
-
-    total_flops = 2 * M * N * K
-    print(f"Best latency: {best_latency}")
-    print(f"Best TFlops: {total_flops / best_latency * 1e-9}")
-    print(f"Best config: {best_config}")
-    # print(f"Ref TFlops: {total_flops / ref_latency * 1e-9}")
+    return best_latency, best_config, ref_latency
 
 
 def test_run_dequantize_gemm():
@@ -521,6 +520,15 @@ def test_run_dequantize_gemm():
 
 
 if __name__ == "__main__":
-    # test_fp4_fp16_convert_close()
-    test_run_dequantize_gemm()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--m', type=int, default=128, help='M')
+    parser.add_argument('--n', type=int, default=8192, help='N')
+    parser.add_argument('--k', type=int, default=8192, help='K')
+    args = parser.parse_args()
+    M, N, K = args.m, args.n, args.k
+    total_flops = 2 * M * N * K
+    best_latency, best_config, ref_latency = tune_gemm(M, N, K, "float16", "float16", "float32")
+    print(f"Best latency: {best_latency}")
+    print(f"Best TFlops: {total_flops / best_latency * 1e-9}")
+    print(f"Best config: {best_config}")
 
