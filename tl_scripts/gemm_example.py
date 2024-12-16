81ce9eadd906ba7dbbfd7b6574a796b6d3b40de2
Yu Cheng <yu.cheng@pku.edu.cn>
2024-12-16 07:58:23 -0800

    update examples

--- tl_scripts/gemm_example.py
+++ tl_scripts/gemm_example.py
@@ -1,85 +1,96 @@
-import argparse
 import torch
 from tvm import tl
 import tvm.tl.language as T
 from tvm.tl.autotuner import *
 import itertools
-
-targetHopper = True
-swizzle_M = 4096
-swizzle_N = 4096
-
-
-def ref_program(A, B):
-    return A @ B
+import argparse
 
 def get_configs():
     block_M = [128]
     block_N = [128, 256]
     block_K = [64]
     num_stages = [2]
-    thread_num = [256]
-    _configs = list(itertools.product(block_M, block_N, block_K, num_stages, thread_num))
+    threads = [256]
+    _configs = list(itertools.product(block_M, block_N, block_K, num_stages, threads))
 
     configs = [
-        {'block_M': c[0], 'block_N': c[1], 'block_K': c[2], 'num_stages': c[3], 'thread_num': c[4]}
+        {'block_M': c[0], 'block_N': c[1], 'block_K': c[2], 'num_stages': c[3], 'threads': c[4]}
         for c in _configs
     ]
     return configs
 
-def matmul(M, N, K):
-    
-    @autotune(configs=get_configs(), keys=['block_M', 'block_N', 'block_K', 'num_stages', 'thread_num'], warmup=10, rep=5)
-    @jit(out_idx=[2], supply_type=tl.TensorSupplyType.Integer, ref_prog=ref_program)
-    def kernel(block_M = None, block_N = None, block_K = None, num_stages = None, thread_num = None):
-        dtype = "float16"
-        accum_dtype = "float"
+def matmul(M, N, K, tune=False):
+    dtype = "float16"
+    accum_dtype = "float"
 
+    def kernel_func(block_M, block_N, block_K, num_stages, threads):
         @T.prim_func
-        def main(A: T.Buffer((M, K), dtype), B: T.Buffer((K, N), dtype), C: T.Buffer((M, N), dtype)): # type: ignore
-            with T.Kernel(T.ceildiv(M, swizzle_M) * T.ceildiv(N, swizzle_N), T.ceildiv(swizzle_N, block_N), T.ceildiv(swizzle_M, block_M), threads=thread_num) as (bx, by, bz):
-                A_shared = T.alloc_shared((block_M, block_K), dtype)
-                B_shared = T.alloc_shared((block_K, block_N), dtype)
-                C_local = T.alloc_fragment((block_M, block_N), accum_dtype)
-                T.clear(C_local)
-                for k in T.Pipelined(T.ceildiv(K, block_K), num_stages=num_stages):
-                    T.copy(A[(bx % T.ceildiv(M, swizzle_M)) * swizzle_M + bz * block_M, k * block_K], A_shared)
-                    T.copy(B[k * block_K, T.floordiv(bx, T.ceildiv(N, swizzle_N)) * swizzle_N + by * block_N], B_shared)
-                    T.gemm(A_shared, B_shared, C_local)
-                T.copy(C_local, C[(bx % T.ceildiv(M, swizzle_M)) * swizzle_M + bz * block_M, T.floordiv(bx, T.ceildiv(N, swizzle_N)) * swizzle_N + by * block_N])
-
-        @T.prim_func
-        def main_hopper(A: T.Buffer((M, K), dtype), B: T.Buffer((K, N), dtype), C: T.Buffer((M, N), dtype)): # type: ignore
-            with T.Kernel(T.ceildiv(M, swizzle_M) * T.ceildiv(N, swizzle_N), T.ceildiv(swizzle_N, block_N), T.ceildiv(swizzle_M, block_M), threads=thread_num) as (bx, by, bz):
+        def main(A: T.Buffer((M, K), dtype), B: T.Buffer((K, N), dtype), C: T.Buffer((M, N), dtype)):
+            with T.Kernel(T.ceildiv(N, block_N), T.ceildiv(M, block_M), threads=threads) as (bx, by):
                 A_shared = T.alloc_shared((block_M, block_K), dtype)
                 B_shared = T.alloc_shared((block_K, block_N), dtype)
                 C_shared = T.alloc_shared((block_M, block_N), dtype)
                 C_local = T.alloc_fragment((block_M, block_N), accum_dtype)
+
+                T.use_swizzle(10)
+                T.annotate_layout({C_shared: tl.layout.make_swizzled_layout(C_shared)})
+
                 T.clear(C_local)
                 for k in T.Pipelined(T.ceildiv(K, block_K), num_stages=num_stages):
-                    T.copy(A[(bx % T.ceildiv(M, swizzle_M)) * swizzle_M + bz * block_M, k * block_K], A_shared)
-                    T.copy(B[k * block_K, T.floordiv(bx, T.ceildiv(N, swizzle_N)) * swizzle_N + by * block_N], B_shared)
+                    T.copy(A[by * block_M, k * block_K], A_shared)
+                    T.copy(B[k * block_K, bx * block_N], B_shared)
                     T.gemm(A_shared, B_shared, C_local)
                 T.copy(C_local, C_shared)
-                T.copy(C_shared, C[(bx % T.ceildiv(M, swizzle_M)) * swizzle_M + bz * block_M, T.floordiv(bx, T.ceildiv(N, swizzle_N)) * swizzle_N + by * block_N])
+                T.copy(C_shared, C[by * block_M, bx * block_N])
+        return main
 
-        if targetHopper:
-            return main_hopper
-        else:
-            return main
-    return kernel()
+    if tune:
+        @autotune(
+            configs=get_configs(),
+            keys=["block_M", "block_N", "block_K", "num_stages", "threads"],
+            warmup=10,
+            rep=10
+        )
+        @jit(out_idx=[2], supply_type=tl.TensorSupplyType.Integer, ref_prog=ref_program, profiler="auto")
+        def kernel(block_M=None, block_N=None, block_K=None, num_stages=None, threads=None):
+            return kernel_func(block_M, block_N, block_K, num_stages, threads)
 
+        return kernel()
+    else:
+        def kernel(block_M, block_N, block_K, num_stages, threads):
+            return kernel_func(block_M, block_N, block_K, num_stages, threads)
+
+        return kernel
+
+def ref_program(A, B):
+    return A @ B
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--m', type=int, default=8192, help='M')
     parser.add_argument('--n', type=int, default=8192, help='N')
     parser.add_argument('--k', type=int, default=8192, help='K')
+    parser.add_argument('--tune', action='store_true', help='tune configs')
     args = parser.parse_args()
     M, N, K = args.m, args.n, args.k
     total_flops = 2 * M * N * K
-    best_latency, best_config, ref_latency = matmul(M, N, K)
-    print(f"Best latency: {best_latency}")
-    print(f"Best TFlops: {total_flops / best_latency * 1e-9}")
-    print(f"Best config: {best_config}")
-    print(f"Ref TFlops: {total_flops / ref_latency * 1e-9}")
+
+    if (not args.tune):
+        program = matmul(M, N, K, tune=args.tune)(block_M=256, block_N=128, block_K=64, num_stages=4, threads=256)
+        mod, params = tl.lower(program)
+        mod = tl.Profiler(mod, params, [2], tl.TensorSupplyType.Normal)
+        mod.assert_allclose(ref_program, rtol=0.01, atol=0.01)
+        print("All checks pass.")
+
+        latency = mod.do_bench(ref_program, warmup=500)
+        print("{:.2f} ms".format(latency))
+        print("{:.2f} TFlops".format(total_flops / latency * 1e-9))
+        latency = mod.do_bench(mod.func, n_warmup=10, n_repeat=10, profiler="torch")
+        print("{:.2f} ms".format(latency))
+        print("{:.2f} TFlops".format(total_flops / latency * 1e-9))
+    else:
+        best_latency, best_config, ref_latency = matmul(M, N, K, tune=args.tune)
+        print(f"Best latency: {best_latency}")
+        print(f"Best TFlops: {total_flops / best_latency * 1e-9}")
+        print(f"Best config: {best_config}")
+        print(f"Ref TFlops: {total_flops / ref_latency * 1e-9}")
